6ac057232f2dfc7bbc1a81bf7c58c264cf5bc46f
Rey Almaraz <ChrisRey@wlan-t-195-63-dhcp.CSUChico.EDU>
2018-02-01 11:51:49 -0800

    Describe what is new in your own words

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,19 @@
 //Authors: 
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main()
 {
+  string name;
+  int number;
+  double decimal;
+  	cout<<" Give me a name \n";
+	cin>>name;
+	cout<<"I need a Number \n";
+	cin>>number;
+	cout<<"This is a Decimal number \n";
+       cin>>decimal;
+cout<<name<<" went to the store and bought "<<number<<" apples and it cost them "<<decimal<<endl;       
 
   return 0;
 }
